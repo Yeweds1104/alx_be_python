a8ef89a9cec0ed60a6fb1ab6a208f5ce4a86a385
Yeweds Dagne <yewedsdagne@gmail.com>
2025-05-22 19:19:37 +0300

    Adding a python file containig basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+var number1 = 10;
+var number2 = 5;
+print("Addition of " + number1 + " and " + number2 + " is " + (number1 + number2));
+print("Subtraction of " + number1 + " and " + number2 + " is " + (number1 - number2));
+print("Multiplication of " + number1 + " and " + number2 + " is " + (number1 * number2));
