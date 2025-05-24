acb92a03fae7082b655176f6ae66c32ed78d83a6
Yeweds Dagne <yewedsdagne@gmail.com>
2025-05-24 08:34:28 +0300

    calculating user's monthly savings and potential future savings

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-income = input("Enter your monthly income: ");
-expense = input("Enter your total monthly expenses: ");
-saving = int(income) - int(expense);
+monthly_income = input("Enter your monthly income: ");
+monthly_expenses = input("Enter your total monthly expenses: ");
+monthly_savings = int(monthly_income) - int(monthly_expenses);
 interest = 0.05;
-annual_saving = saving * 12 + saving * 12 * interest;
-print("Your monthly savings are $", saving);
-print("Projected savings after one year, with interest, is: $", annual_saving);
+projected_saving = monthly_savings * 12 + monthly_savings * 12 * interest;
+print("Your monthly savings are $", monthly_savings);
+print("Projected savings after one year, with interest, is: $", projected_saving);
