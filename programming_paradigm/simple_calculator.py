f61a581fa5fd7a746299aaf3368551bd074012d7
Yeweds Dagne <yewedsdagne@gmail.com>
2025-06-10 16:34:37 +0300

    Writing unit test for a simple calculator class

--- programming_paradigm/simple_calculator.py
+++ programming_paradigm/simple_calculator.py
@@ -1,11 +1,11 @@
 class SimpleCalculator:
-    def add(a, b):
+    def add(self, a, b):
         return a + b
-    def subtract(a, b):
+    def subtract(self, a, b):
         return a - b
-    def multiply(a, b):
+    def multiply(self, a, b):
         return a * b
-    def divide(a, b):
+    def divide(self, a, b):
         if b == 0:
             raise ValueError("Can not divide by zero.")
         return a / b
--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,20 +1,18 @@
 import unittest
-from simple_calculator import SimpleCalculator
+from simple_calculator import SimpleCalculator 
 
 class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
     def test_add(self):
-        result = SimpleCalculator.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(self.calc.add(10, 5), 15)
     def test_subtract(self):
-        result = SimpleCalculator.subtract(10, 5)
-        self.assertEqual(result, 5)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
     def test_multiply(self):
-        result = SimpleCalculator.multiply(10, 5)
-        self.assertEqual(result, 50)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
     def test_divide(self):
-        result = SimpleCalculator.divide(10, 5)
-        self.assertEqual(result, 2.0)
-        self.assertRaises(ValueError, SimpleCalculator.divide, 10, 0)
+        self.assertEqual(self.calc.divide(10, 5), 2.0)
+        self.assertRaises(ValueError, self.calc.divide, 10, 0)
         
 if __name__ == "__main__":
     unittest.main()
